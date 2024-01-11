4822fc696b30ebb76813a6e1525f10a0dd47da2e
samson harize <samson.harize@my-digital-school.org>
2024-01-11 13:47:40 +0100

    correction et css

--- controllers/Blog.controller.php
+++ controllers/Blog.controller.php
@@ -2,16 +2,16 @@
 require_once('models/PostManager.class.php');
 class BlogController
 {
-    private $postManager; // permet d'accéder aux méthodes de la classe PostManager
+    private $PostManager; // permet d'accéder aux méthodes de la classe PostManager
     public function __construct()
     {
-        $this->postManager = new PostManager();
-        $this->postManager->getPostsFromDb();
+        $this->PostManager = new PostManager();
+        $this->PostManager->getPostsFromDb();
     }
     public function displayPosts()
     {
         global $posts; // on récupère la variable globale $posts en la créant dans l'espace de nom global
-        $posts = $this->postManager->getPosts(); // on récupère les posts depuis la base de données
+        $posts = $this->PostManager->getPosts(); // on récupère les posts depuis la base de données
         require_once('views/blog.view.php');
     }
 }
--- views/accueil.view.php
+++ views/accueil.view.php
@@ -1,7 +1,58 @@
 <?php ob_start(); // start c'est comme une banane dans le pot d'échappement de PHP
 ?>
 <h1>Index (accueil)</h1>
+<section>
+        <h2>Expérience professionnelle</h2>
+        <h3>Ingénieur logiciel chez XYZ Company</h3>
+        <p>Date de début - Date de fin</p>
+        <ul>
+            <li>Responsabilité 1</li>
+            <li>Responsabilité 2</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Éducation</h2>
+        <h3>Master en Informatique à l'Université ABC</h3>
+        <p>Date de début - Date de fin</p>
+    </section>
+
+    <section>
+        <h2>Compétences techniques</h2>
+        <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>PHP, MySQL</li>
+            <li>Frameworks: Laravel, Symfony</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Projets</h2>
+        <h3>Projet 1</h3>
+        <p>Description du projet 1.</p>
+        <h3>Projet 2</h3>
+        <p>Description du projet 2.</p>
+    </section>
+
+    <section>
+        <h2>Langues</h2>
+        <ul>
+            <li>Français (Langue maternelle)</li>
+            <li>Anglais (Courant)</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Coordonnées</h2>
+        <p>Email: john.doe@email.com</p>
+        <p>Téléphone: +123 456 789</p>
+        <p>Adresse: 123 Rue de l'Exemple, Ville</p>
+    </section>
+
+
+
 <?php
 $title = 'Accueil - site de YY';
 $content = ob_get_clean(); // clean c'est comme retirer la banane du pot d'échappement
 require_once 'template.php';
+
